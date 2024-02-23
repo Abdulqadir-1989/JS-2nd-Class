90c9bcb4f5baf61b33fc5cf9b4f627e70ebafca2
Abdul Qadir <157492762+Abdulqadir-1989@users.noreply.github.com>
2024-02-23 18:37:59 +0500

    initial work

---
+++ Class-2/app.js
@@ -0,0 +1,24 @@
+var num1 = +prompt("Enter value 1")
+var num2 = +prompt("Enter value 2")
+var result = (num1 + num2)
+
+document.write("The Addition of" + num1 + "and" + num2 + "is" + "=" + result)
+
+
+var num1 = +prompt("Enter value 1")
+var num2 = +prompt("Enter value 2")
+var result = (num1 - num2)
+
+document.write("The Substraction of" + num1 + "and" + num2 + "is" + "=" + result)
+
+var num1 = +prompt("Enter value 1")
+var num2 = +prompt("Enter value 2")
+var result = (num1 * num2)
+
+document.write("The Multiplication of" + num1 + "and" + num2 + "is" + "=" + result)
+
+var num1 = +prompt("Enter value 1")
+var num2 = +prompt("Enter value 2")
+var result = (num1 / num2)
+
+document.write("The Division of" + num1 + "and" + num2 + "is" + "=" + result)
